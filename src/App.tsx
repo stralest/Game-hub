3f9536f18ead0e216573a2c40f8f92c485ac1e94
Strahinja <moep071@gmail.com>
2026-02-16 20:49:11 +0100

    Add a dynamic page heading

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import type { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import type { Platform } from "./hooks/usePlatforms";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -48,6 +49,7 @@ function App() {
           </GridItem>
         </Show>
         <GridItem area="main">
+          <GameHeading gameQuery={gameQuery} />
           <HStack spacing={5} paddingLeft={5}>
             <PlatformSelector
               selectedPlatform={gameQuery.platform}
